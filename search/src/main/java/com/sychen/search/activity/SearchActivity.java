8687c4194d9a422d814c93d398f4b828e23d5bf5
fujie33 <71702859+fujie33@users.noreply.github.com>
2021-06-22 09:50:11 +0800

    6/22

--- search/src/main/java/com/sychen/search/activity/SearchActivity.java
+++ search/src/main/java/com/sychen/search/activity/SearchActivity.java
@@ -1,20 +1,52 @@
 package com.sychen.search.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
+import com.alibaba.android.arouter.facade.annotation.Autowired;
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.sychen.basic.ARouterUtil;
 import com.sychen.basic.activity.BaseActivity;
 import com.sychen.search.R;
+import com.sychen.search.adapter.SearchAdapter;
+import com.sychen.search.adapter.SearchDiffCallBack;
+import com.sychen.search.network.model.SearchNews;
+import com.sychen.search.network.viewmodel.SearchViewModel;
+
+import java.util.List;
 
 @Route(path = ARouterUtil.START_SEARCH_ACTIVITY)
 public class SearchActivity extends BaseActivity {
-
+    private SearchViewModel viewModel;
+    private SearchAdapter searchAdapter;
+    private RecyclerView recyclerView;
+    @Autowired(name = "SEARCH_TEXT")
+    String searchText;
+    
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        viewModel = new ViewModelProvider(this).get(SearchViewModel.class);
+        initViews();
+    }
+
+    private void initViews() {
+        recyclerView = findViewById(R.id.search_recyclerView);
+        searchAdapter = new SearchAdapter(new SearchDiffCallBack());
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+        recyclerView.setAdapter(searchAdapter);
+        viewModel.searchNews(searchText);
+        viewModel.searchNews.observe(this,
+                searchNews -> searchAdapter.submitList(searchNews)
+        );
     }
 }
